1882c007f93087fa21e2797245c2dc7c24a1ab31
josecipriano <ciprianoaceytunoj@gmail.com>
2024-07-19 21:47:36 -0600

    avances

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+import org.josecipriano.webapp.model.Producto;
+import org.josecipriano.webapp.util.JpaUtil;
+
+
+public class Main {
+    private static EntityManager em = JpaUtil.getEntityManager();
+            
+    
+    public static void main (String [] args){
+        TypedQuery<Producto> query = em.createQuery("SELECT p FROM Producto p", Producto.class);
+        List<Producto> productos = query.getResultList();
+        
+        productos.forEach(producto -> System.out.println(producto));
+    }
+}
